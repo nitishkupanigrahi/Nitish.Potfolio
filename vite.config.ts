c88afc7f656f8121211b91af7fa0042f3859a3f5
nitishkupanigrahi <146910828+nitishkupanigrahi@users.noreply.github.com>
2026-03-11 14:07:48 +0530

    Update vite.config.ts
    allowing to publish

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true
+  },
   preview: {
     host: true,
-    port: process.env.PORT,
+    port: process.env.PORT || 4173,
     allowedHosts: ["nitish-potfolio-4.onrender.com"]
   }
 });
